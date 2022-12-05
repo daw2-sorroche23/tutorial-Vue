d8d75d836ac8bdbd1c0c399c4213e069044baee5
daw2-sorroche23 <sorrocheperezzeus@fpllefia.com>
2022-12-05 18:22:34 +0100

    Primer commit

---
+++ src/components/tablaPersona.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="tabla-personas">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jon</td>
+            <td>Nieve</td>
+            <td>jon@email.com</td>
+          </tr>
+          <tr>
+            <td>Tyrion</td>
+            <td>Lannister</td>
+            <td>tyrion@email.com</td>
+          </tr>
+          <tr>
+            <td>A</td>
+            <td>Daenerys</td>
+            <td>daenerys@email.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: 'tabla-persona',
+      props: {
+        persona: Array,
+      },
+    }
+  </script>
+  
+  <style scoped></style>
